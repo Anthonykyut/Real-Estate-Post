ec2db6990acb4ad41eda5beda0ea05f89556cd97
Anthonykyut <pako79096@gmail.com>
2025-03-18 17:53:27 +0800

    initial commit

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { LogOut, Pencil } from 'lucide-react'
 import Link from 'next/link'
-
+import Image from 'next/image'
+import profile from '@/assets/profile.png'
 
 const Profile = () => {
   return (
     <div className='w-[19vw] h-50 shadow-xl bg-gray-200 flex-col items-center flex p-5 rounded-md justify-between'>
-        <div> <img src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp' className='w-15 h-15 rounded-full'></img></div> 
+        <div> <Image src={profile} className='w-15 h-15 rounded-full' alt={''}/></div> 
         <h3 className='font-semibold text-[1rem]'> Anthony Balderas </h3>
         <h3 className=' text-[0.6rem] text-gray-400'> anthonykyut@gmail.com </h3>
        
